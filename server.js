aa9ec12f7d5b5c48a82a78231342d2bca5d702ba
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-05-21 19:27:24 +0900

    firstcommit
    첨

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const port = 3000;
+
+app.use(cors()); //응답요청수락
+
+//http메소드, 라우팅,콜백함수
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
