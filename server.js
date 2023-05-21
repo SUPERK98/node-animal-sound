29a0420f705d51a79ccee71c38ed053813dc34d9
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-05-21 19:47:17 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors()); //응답요청수락
 
 //http메소드, 라우팅,콜백함수
-app.get("/", function (req, res) {
+app.get("", function (req, res) {
   res.sendFile(__dirname + "/map.html");
 });
 
