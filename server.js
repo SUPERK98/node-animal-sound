724009e612f549aa1f862a839263786bb467f3a5
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-05-21 19:31:28 +0900

    renew
    맵페이지버전

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors()); //응답요청수락
 
 //http메소드, 라우팅,콜백함수
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/map.html");
 });
 
 app.listen(port, () => {
